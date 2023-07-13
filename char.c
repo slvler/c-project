764b1589e303bcfe9c91b96a55943e0919736538
slvler <56368718+slvler@users.noreply.github.com>
2023-07-13 23:59:50 +0300

    Add files via upload
    char variable

---
+++ char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("\n");
+    char name[20] = "My Name Is Red";
+    char author[15] = "Orhan Pamuk";
+    char type[15] = "Adventure";
+    char page[5] = "671";
+    char language[10] = "english";
+    char publisher[15] = "Faber And Faber";
+
+
+    printf("%s\n", name);
+    printf("%s\n", author);
+    printf("%s\n", type);
+    printf("%s\n", page);
+    printf("%s\n", language);
+    printf("%s\n", publisher);
+    return 0;
+}
