fc6de83c68c886265bf0b3a583db20fb7f93bf3b
slvler <56368718+slvler@users.noreply.github.com>
2023-07-13 23:34:53 +0300

    Add files via upload
    init

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("******************\n");
+    printf("\n\n");
+    printf("Hello, World!\n");
+    printf("hello!\n");
+    printf("developer\n");
+    printf("hello c");
+    printf("\n\n");
+    printf("******************\n");
+    return 0;
+}
