89a1788901969705c39f6956a8b8bd7467b08a12
slvler <56368718+slvler@users.noreply.github.com>
2023-07-14 01:07:17 +0300

    Add files via upload

---
+++ int.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main() {
+
+//    int number1, number2, total, diff;
+//    number1 = 40;
+//    number2 = 80;
+//    total = number1 + number2;
+//    printf("Total: %d", total);
+
+
+//    printf("enter the first number: ");
+//    scanf("%d", &number1);
+//    printf("enter: %d", number1);
+
+
+//    printf("enter value first: ");
+//    scanf("%d", &number1);
+//    printf("enter value second: ");
+//    scanf("%d", &number2);
+//
+//    total = number1 + number2;
+//    diff = number1 - number2;
+//
+//    printf("Total: %d\n", total);
+//    printf("diff: %d", diff);
+
+
+    /*
+     * area perimeter calculation
+     */
+//    int edge, area, environment;
+//
+//    printf("square area and perimeter calculation\n\n");
+//
+//    printf("enter edge: ");
+//    scanf("%d", &edge);
+//
+//    area=edge*edge;
+//    environment=edge*4;
+//
+//    printf("area of the square %d\n", area);
+//    printf("perimeter of the square %d", environment);
+
+
+    int first_exam,second_exam,third_exam,average;
+
+    printf("first exam: ");
+    scanf("%d", &first_exam);
+
+    printf("second exam: ");
+    scanf("%d", &second_exam);
+
+    printf("third exam: ");
+    scanf("%d", &third_exam);
+
+    average= (first_exam+second_exam+third_exam)/3;
+
+    printf("total: %d", average);
+
+
+
+
+    return 0;
+}
